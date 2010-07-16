d04d4cd6e9ab8c12349e6e46e28918b757776f57
Markus Flambard <markus@flambard.se>
2010-07-16 15:12:55 +0200

    Added the BERT-TRANSLATABLE type.

--- bert.lisp
+++ bert.lisp
@@ -5,6 +5,22 @@
 
 (in-package :bert)
 
+
+(deftype bert-translatable ()
+  "A type that encompasses all types of Lisp objects that can be translated to BERT objects."
+  '(satisfies bert-translatable-p))
+
+(defun bert-translatable-p (object)
+  "Returns true if OBJECT is translatable to an Erlang object."
+  (typecase object
+    ((or integer float symbol string hash-table erlang-tuple erlang-binary time regex)
+     t)
+    (list
+     (every #'bert-translatable-p object))
+    (t
+     nil)))
+
+
 (defmethod encode (object)
   (cleric:encode object :version-tag t))
 
