e45521dc5f8af2eee2f6d723b6e15b4bfedb1461
Markus Flambard <markus.flambard@klarna.com>
2011-05-19 07:31:24 +0200

    Refactored epmd.lisp

--- src/epmd.lisp
+++ src/epmd.lisp
@@ -63,6 +63,80 @@
     (end-of-file () (error 'connection-closed-error))))
 
 
+;;;
+;;; PORT_PLEASE2_REQ
+;;
+;; 2 bytes: Total length of following message
+;; 1 byte:  'z'            [PORT_PLEASE2_REQ message]
+;; N bytes: Node name
+;;
+
+(defun make-port-please2-request (node-name)
+  (concatenate 'vector
+               (uint16-to-bytes (1+ (length node-name)))
+               (vector +port-please2-req+)
+               (string-to-bytes node-name)))
+
+
+;;;
+;;; PORT_PLEASE2_RESP
+;;
+;; 1 byte:  'w'            [PORT2_RESP message]
+;; 1 byte:  Result         [0 means OK, >0 means ERROR]
+;;; Continued only if result = 0
+;; 2 bytes: Port
+;; 1 byte:  Node type      [77 means Erlang node, 72 means hidden node]
+;; 1 byte:  Protocol       [0 means TCP/IP v4]
+;; 2 bytes: Lowest version supported
+;; 2 bytes: Highest version supported
+;; 2 bytes: Node name length
+;; N bytes: Node name
+;; 2 bytes: Extra field length
+;; M bytes: Extra field
+;;
+
+(defun read-port-please2-response (stream host)
+  (handler-case
+      (let ((tag (read-byte stream))
+            (result (read-byte stream)))
+        (cond
+          ((/= tag +port2-resp+)
+           (error 'unexpected-message-tag-error
+                  :tag tag
+                  :expected-tags (list +port2-resp+)))
+          ((/= 0 result)
+           nil) ;; No nodes with that name.
+          (t
+           (let* ((port (read-uint16 stream))
+                  (node-type (read-byte stream))
+                  (protocol (read-byte stream))
+                  (lowest-version-supported (read-uint16 stream))
+                  (highest-version-supported (read-uint16 stream))
+                  (node-name-length (read-uint16 stream))
+                  (node-name (read-bytes-as-string node-name-length epmd))
+                  (extra-field-length (read-uint16 epmd))
+                  (extra-field (read-bytes extra-field-length epmd)))
+             (make-instance 'remote-node
+                            :port port
+                            :node-type (case node-type
+                                         (#.+node-type-hidden+ 'hidden)
+                                         (#.+node-type-erlang+ 'erlang)
+                                         (otherwise
+                                          (error 'malformed-message-error
+                                                 :bytes port2-response)))
+                            :protocol protocol
+                            :lowest-version lowest-version-supported
+                            :highest-version highest-version-supported
+                            :name node-name
+                            :host host
+                            :extra-field extra-field) ))))
+    (end-of-file () (error 'connection-closed-error))))
+
+
+;;;
+;;; EPMD API
+;;;
+
 (defun epmd-publish (port &optional (node-name "lispnode"))
   ;; TODO: Check if we are already registered
   (let* ((socket (handler-case
@@ -78,25 +152,24 @@
       (setf *epmd-socket* socket)
       t)))
 
+
 (defun epmd-unpublish ()
   (when *epmd-socket*
     (usocket:socket-close *epmd-socket*)
     t))
 
-(defmacro with-epmd-connection-stream ((stream-var &optional (host "localhost") (port +epmd-port+)) &body body)
-  "Create a local scope where STREAM-VAR is a socket stream connected to the EPMD."
-  (let ((socket-var (gensym)))
-    `(let* ((,socket-var (handler-case (usocket:socket-connect
-                                        ,host
-                                        ,port
-                                        :element-type '(unsigned-byte 8))
-                           (usocket:connection-refused-error ()
-                             (error 'epmd-unreachable-error))
-                           (usocket:unknown-error ()
-                             (error 'epmd-host-unknown-error))))
-            (,stream-var (usocket:socket-stream ,socket-var)))
-       (unwind-protect (progn ,@body)
-         (usocket:socket-close ,socket-var))) ))
+
+(defun epmd-lookup-node (node-name &optional (host "localhost"))
+  "Query the EPMD about a node. Returns a REMOTE-NODE object that represents the node."
+  (with-epmd-connection-stream (epmd host)
+    (write-sequence (make-port-please2-request node-name) epmd)
+    (finish-output epmd)
+    (read-port-please2-response stream host)))
+
+
+;;;
+;;; Helper functions
+;;;
 
 (defun node-name (node-string)
   "Return the name part of a node identifier"
@@ -114,61 +187,17 @@
         (subseq node-string (1+ pos))
         "localhost"))) ;; OK with localhost??
 
-(defun epmd-lookup-node (node-name &optional (host "localhost"))
-  "Query the EPMD about a node. Returns a REMOTE-NODE object that represents the node."
-  (with-epmd-connection-stream (epmd host)
-    ;;;; PORT_PLEASE2_REQ ;;;;
-    ;; 2 bytes: Total length of following message
-    ;; 1 byte:  'z'            [PORT_PLEASE2_REQ message]
-    ;; N bytes: Node name
-    (let* ((message-length (1+ (length node-name)))
-           (port-please2-req (concatenate 'vector
-                                          (uint16-to-bytes message-length)
-                                          (vector +port-please2-req+)
-                                          (string-to-bytes node-name))))
-      (write-sequence port-please2-req epmd)
-      (finish-output epmd))
-    ;;;; PORT2_RESP ;;;;
-    ;; 1 byte:  'w'            [PORT2_RESP message]
-    ;; 1 byte:  Result         [0 means OK, >0 means ERROR]
-    ;;; Continued only if result = 0
-    ;; 2 bytes: Port
-    ;; 1 byte:  Node type      [77 means Erlang node, 72 means hidden node]
-    ;; 1 byte:  Protocol       [0 means TCP/IP v4]
-    ;; 2 bytes: Lowest version supported
-    ;; 2 bytes: Highest version supported
-    ;; 2 bytes: Node name length
-    ;; N bytes: Node name
-    ;; 2 bytes: Extra field length
-    ;; M bytes: Extra field
-    (handler-case
-        (let ((tag (read-byte epmd))
-              (result (read-byte epmd)))
-          (cond
-            ((/= tag +port2-resp+)
-             (error 'unexpected-message-tag-error
-                    :tag tag
-                    :expected-tags (list +port2-resp+)))
-            ((/= 0 result)
-             nil) ;; No nodes with that name.
-            (t
-             (let* ((port2-response (read-bytes 10 epmd))
-                    (node-name-length (bytes-to-uint16 (subseq port2-response 8)))
-                    (node-name (bytes-to-string (read-bytes node-name-length epmd)))
-                    (extra-field-length (bytes-to-uint16 (read-bytes 2 epmd)))
-                    (extra-field (read-bytes extra-field-length epmd)))
-               (make-instance 'remote-node
-                              :port (bytes-to-uint16 port2-response)
-                              :node-type (case (aref port2-response 2)
-                                           (#.+node-type-hidden+ 'hidden)
-                                           (#.+node-type-erlang+ 'erlang)
-                                           (otherwise
-                                            (error 'malformed-message-error
-                                                   :bytes port2-response)))
-                              :protocol (aref port2-response 3)
-                              :lowest-version (bytes-to-uint16 (subseq port2-response 4 6))
-                              :highest-version (bytes-to-uint16 (subseq port2-response 6 8))
-                              :name node-name
-                              :host host
-                              :extra-field extra-field) ))))
-      (end-of-file () (error 'connection-closed-error))) ))
+(defmacro with-epmd-connection-stream ((stream-var &optional (host "localhost") (port +epmd-port+)) &body body)
+  "Create a local scope where STREAM-VAR is a socket stream connected to the EPMD."
+  (let ((socket-var (gensym)))
+    `(let* ((,socket-var (handler-case (usocket:socket-connect
+                                        ,host
+                                        ,port
+                                        :element-type '(unsigned-byte 8))
+                           (usocket:connection-refused-error ()
+                             (error 'epmd-unreachable-error))
+                           (usocket:unknown-error ()
+                             (error 'epmd-host-unknown-error))))
+            (,stream-var (usocket:socket-stream ,socket-var)))
+       (unwind-protect (progn ,@body)
+         (usocket:socket-close ,socket-var))) ))
