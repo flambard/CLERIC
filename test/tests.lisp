f3ef4c0299d4a317ca575e24f3768557cb119a12
Markus Flambard <markus@flambard.se>
2013-07-06 16:03:01 +0200

    Fixed bug: MD5:MD5SUM-SEQUENCE is deprecated for strings, use MD5:MD5SUM-STRING instead.

--- test/tests.lisp
+++ test/tests.lisp
@@ -20,3 +20,13 @@
 
 (defmethod asdf:perform ((op asdf:test-op) (system (eql (asdf:find-system :cleric-test))))
   (run-all-tests))
+
+
+(test sbcl-challenge-md5-digest-bug
+    ;; Bug in CLERIC::CALCULATE-DIGEST, it returned same MD5sum for different
+    ;; strings! Turns out that MD5:MD5SUM-SEQUENCE is deprecated for strings.
+    ;; Use MD5:MD5SUM-STRING instead.
+    (is (not (every #'=
+                    (cleric::calculate-digest 1234567890 "kaka")
+                    (cleric::calculate-digest 1234567891 "kaka"))))
+  )
