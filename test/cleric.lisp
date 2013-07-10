ddfbabddb448b44c4158816cdfe7bf8cc0a2b6c7
Markus Flambard <markus@flambard.se>
2013-07-10 22:14:14 +0200

    Moved unit test from test.lisp to cleric.lisp to include it into the cleric test suite.

--- test/cleric.lisp
+++ test/cleric.lisp
@@ -2,3 +2,13 @@
 
 #+nil(def-suite cleric :in cleric)
 (in-suite cleric)
+
+
+(test sbcl-challenge-md5-digest-bug
+    ;; Bug in CLERIC::CALCULATE-DIGEST, it returned same MD5sum for different
+    ;; strings! Turns out that MD5:MD5SUM-SEQUENCE is deprecated for strings.
+    ;; Use MD5:MD5SUM-STRING instead.
+    (is (not (every #'=
+                    (cleric::calculate-digest 1234567890 "kaka")
+                    (cleric::calculate-digest 1234567891 "kaka"))))
+  )
