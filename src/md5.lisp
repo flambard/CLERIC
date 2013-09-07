2e44f8ac97d70c2381f82dd42450afae10114f1b
Markus Flambard <markus@flambard.se>
2013-09-08 00:23:35 +0200

    Make CALCULATE-DIGEST work for all CL implementations.

--- src/md5.lisp
+++ src/md5.lisp
@@ -3,4 +3,8 @@
 (in-package :cleric)
 
 (defun calculate-digest (challenge cookie)
-  (md5:md5sum-string (format nil "~a~a" cookie challenge)))
+  (let ((input (format nil "~a~a" cookie challenge)))
+    #+(or :cmu :sbcl (and :lispworks (not :lispworks4)) :ccl :allegro)
+    (md5:md5sum-string input)
+    #-(or :cmu :sbcl (and :lispworks (not :lispworks4)) :ccl :allegro)
+    (md5:md5sum-sequence (map '(vector (unsigned-byte 8)) #'char-code input))))
